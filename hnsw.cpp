7c317786ae3a5a7b01113588c0713f181e67fd4d
Karthik-Kundurthy <karthikkundurthy@gmail.com>
2024-11-04 11:09:41 -0500

    hnsw: define DataSet and graph node definitions to help in HNSW implementation

--- hnsw.cpp
+++ hnsw.cpp
@@ -40,6 +40,52 @@ struct DataPoint {
 
 };
 
+struct HNSWNode {
+    DataPoint data_point;
+    int max_level;
+    std::vector<std::vector<HNSWNode*>> connections; 
+    
+
+    HNSWNode(const DataPoint& data_point, int max_level) :  data_point(data_point), max_level(max_level), connections(max_level + 1) {}
+
+    bool layer_in_bounds(int layer) {
+        return layer >= 0 && layer <= max_level;
+    }
+
+    void addConnection(HNSWNode* neighbor, uint64_t layer) {
+        if (layer_in_bounds(layer)) {
+            connections[layer].push_back(neighbor);
+        }
+
+    }
+
+};
+
+// Manage the entire dataset: SIFT and GLOVE
+class DataSet {
+    public:
+        std::string name;
+        std::vector<DataPoint> dataset;
+
+        DataSet(std::string name) : name(name) {}
+
+        void addDataPoint(const DataPoint& data_point) {
+            dataset.push_back(data_point);
+        }
+
+        // void removeDataPoint(const DataPoint&) {
+            
+        // }
+};
+
+
+class HNSW {
+    public:
+
+
+
+};
+
 
 int main(int argc, char* argv[]) {
     UNUSED(argc);
