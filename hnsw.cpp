bfe09d2cb1e64cea12ca29f43994b0b1fccbdb01
Karthik-Kundurthy <karthikkundurthy@gmail.com>
2024-11-04 18:18:36 -0500

    hnsw: Added insertion logic
    Signed-off-by: Karthik-Kundurthy <karthikkundurthy@gmail.com>

--- hnsw.cpp
+++ hnsw.cpp
@@ -81,101 +81,141 @@ class DataSet {
 
 class HNSW {
     public:
+
+        int max_level = 0; // initially at 0 bc no nodes in the graph yet
+        HNSWNode* entry_point = nullptr;
+        float m_l; // normalization factor
     
-    // m_l: normalization factor is an attribute
+        HNSW(float m_l) : m_l(m_l) {}
 
-    /// ALGORITHM 1
-    /// @param q: the query vector
-    /// @param m: the number of established connections
-    /// @param m_max: the maximum number of connectiosn for each element per layer
-    /// @param ef_construction: size of dynamic candidate list
-    /// update the HNSW graph
-    /// 
-    void insert(DataPoint q, uint64_t m, uint64_t m_max, size_t ef_construction) {
-        UNUSED(q);
-        UNUSED(m);
-        UNUSED(m_max);
-        UNUSED(ef_construction);
-
-
-
-    }
-
-    /// ALGORITHM 2
-    /// @param q: the query vector
-    /// @param ep: enter points ep
-    /// @param ef: number of nearest to q elements to return
-    /// @param l_c: layer number
-    /// @return ef closest neighbors to q
-    std::vector<HNSWNode*> search_layer(DataPoint q, std::vector<HNSW> ep, uint64_t ef, size_t l_c) {
-        UNUSED(q);
-        UNUSED(ep);
-        UNUSED(ef);
-        UNUSED(l_c);
-
-        return std::vector<HNSWNode*>();
+        /// ALGORITHM 1
+        /// @param q: the query vector
+        /// @param m: the number of established connections
+        /// @param m_max: the maximum number of connectiosn for each element per layer
+        /// @param ef_construction: size of dynamic candidate list
+        /// update the HNSW graph
+        /// 
+        void insert(DataPoint q, uint64_t m, uint64_t m_max, size_t ef_construction) {
+            UNUSED(q);
+            UNUSED(m);
+            UNUSED(m_max);
+            UNUSED(ef_construction);
 
 
-    }
+            int l = getRandomLevel();
+            int L = max_level;
+            HNSWNode* ep = new HNSWNode(q,l);
 
-    /// ALGORITHM 3
-    /// @param q: base element
-    /// @param C:  candidate elements
-    /// @param M: number of neighbors to return
-    std::vector<HNSWNode*> select_neighbors_simple(DataPoint q, std::vector<HNSWNode> C, uint64_t M) {
-        UNUSED(q);
-        UNUSED(C);
-        UNUSED(M);
+            // entry point has not yet been initialized
+            if (entry_point == nullptr) {
+                entry_point = ep;
+                max_level = l; 
+            } else {
 
+                for (int l_c = L; l_c >= l + 1; l_c--) {
+                    std::vector<HNSWNode*> W = search_layer(q,std::vector<HNSW*>(),1,l_c);
+                    ep = getNearest(W,q);
+                }
+                
+                for (int l_c = std::min(L,l); l_c >= 0; l_c --) {
+                    std::vector<HNSWNode*> W = search_layer(q,std::vector<HNSW*>(),ef_construction,l_c);
+                    std::vector<HNSWNode*> neighbors = select_neighbors_simple(q,W,m); // using the simple algorithm for now
 
-        return std::vector<HNSWNode*>();
-    }
+                    // add bidirectionall connectionts from neighbors to q at layer lc
+                    for (auto* neighbor: neighbors) {
+                        ep -> addConnection(neighbor,l);
+                        neighbor -> addConnection(ep,l);
+                    }
 
-    /// ALGORITHM 4
-    /// @param q query vector
-    /// @param C candidate elements
-    /// @param M: number of neighbors to return
-    /// @param extendCandidates whether or not to extend candidate list
-    /// @param keepPrunedConnections whether or not to add discarded elements
-    /// @param 
-    std::vector<HNSWNode*> select_neighbors_heuristic(DataPoint q, std::vector<HNSWNode> C, uint64_t M, size_t l_c, bool extendCandidates=false, bool keepPrunedConnections=false) {
+                }
 
-        UNUSED(q);
-        UNUSED(C);
-        UNUSED(M);
-        UNUSED(l_c);
-        UNUSED(extendCandidates);
-        UNUSED(keepPrunedConnections);
+                if (l > max_level ) {
+                    max_level = l;
+                    entry_point = ep;
+                }
 
-
-        return std::vector<HNSWNode*>();
-
-    }
-
-    /// ALGORITHM 5
-    /// @param q: the query vector
-    /// @param k: the number of nearest neighbors to return
-    /// @param ef: size of dynamic candidate list
-    /// @return K nearest elements to query vector in the dynamic candidate list
-    ///
-    std::vector<HNSWNode*> knn_search(DataPoint q, uint64_t k, uint64_t ef) {
-        UNUSED(q);
-        UNUSED(k);
-        UNUSED(ef);
-
-        return std::vector<HNSWNode*>();
-    }   
+            }
 
 
 
+        }
+
+        /// ALGORITHM 2
+        /// @param q: the query vector
+        /// @param ep: enter points ep
+        /// @param ef: number of nearest to q elements to return
+        /// @param l_c: layer number
+        /// @return ef closest neighbors to q
+        std::vector<HNSWNode*> search_layer(DataPoint q, std::vector<HNSW*> ep, uint64_t ef, size_t l_c) {
+            UNUSED(q);
+            UNUSED(ep);
+            UNUSED(ef);
+            UNUSED(l_c);
+
+            return std::vector<HNSWNode*>();
 
 
+        }
+
+        /// ALGORITHM 3
+        /// @param q: base element
+        /// @param C:  candidate elements
+        /// @param M: number of neighbors to return
+        std::vector<HNSWNode*> select_neighbors_simple(DataPoint q, std::vector<HNSWNode*> C, uint64_t M) {
+            UNUSED(q);
+            UNUSED(C);
+            UNUSED(M);
+
+            // default value
+            return std::vector<HNSWNode*>();
+        }
+
+        /// ALGORITHM 4
+        /// @param q query vector
+        /// @param C candidate elements
+        /// @param M: number of neighbors to return
+        /// @param extendCandidates whether or not to extend candidate list
+        /// @param keepPrunedConnections whether or not to add discarded elements
+        /// @param 
+        std::vector<HNSWNode*> select_neighbors_heuristic(DataPoint q, std::vector<HNSWNode> C, uint64_t M, size_t l_c, bool extendCandidates=false, bool keepPrunedConnections=false) {
+
+            UNUSED(q);
+            UNUSED(C);
+            UNUSED(M);
+            UNUSED(l_c);
+            UNUSED(extendCandidates);
+            UNUSED(keepPrunedConnections);
 
 
+            return std::vector<HNSWNode*>();
+
+        }
+
+        /// ALGORITHM 5
+        /// @param q: the query vector
+        /// @param k: the number of nearest neighbors to return
+        /// @param ef: size of dynamic candidate list
+        /// @return K nearest elements to query vector in the dynamic candidate list
+        ///
+        std::vector<HNSWNode*> knn_search(DataPoint q, uint64_t k, uint64_t ef) {
+            UNUSED(q);
+            UNUSED(k);
+            UNUSED(ef);
+
+            return std::vector<HNSWNode*>();
+        }   
 
 
+        int getRandomLevel() {
+            return std::floor(-std::log((static_cast<float>(rand())) / RAND_MAX) / std::log(m_l));
+        }
 
+        HNSWNode* getNearest(std::vector<HNSWNode*> W, DataPoint q) {
+            UNUSED(W);
+            UNUSED(q);
+            return W[0];
 
+        }
 };
 
 
