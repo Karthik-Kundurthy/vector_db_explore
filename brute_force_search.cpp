f402ff1b562f181f0a465f68151f18d5aa5d8f54
Karthik-Kundurthy <karthikkundurthy@gmail.com>
2024-11-07 15:01:48 -0500

    Initialize file containing naive vector search logic

---
+++ brute_force_search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <map>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <sstream>
+#include <thread>
+#include <queue>
+#include <optional>
+#include <mutex>
+#include <shared_mutex>
+#include <cassert>
+#include <cstring> 
+#include <exception>
+#include <atomic>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <random>
+#include <limits>
+#include <algorithm>
+
+
+int main(int argc, char* argv[]) {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    return 0;
+
+}
